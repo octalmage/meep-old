2648d557704a88d3660046232225de04dcdff650
Jason Stallings <jason@stallin.gs>
2021-05-29 16:54:45 -0500

    feat: use real domains.

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 pushd vue/node_modules/@starport
-find . -type f -name '*.js' -exec sed -i '' 's/http:\/\/localhost:/https:\/\/meep.social:/g' {} \;
+find . -type f -name '*.js' -exec sed -i '' 's/http:\/\/localhost:26657/https:\/\/rpc.meep.social/g' {} \;
+find . -type f -name '*.js' -exec sed -i '' 's/http:\/\/localhost:1317/https:\/\/api.meep.social/g' {} \;
+find . -type f -name '*.js' -exec sed -i '' 's/http:\/\/localhost:12345/https:\/\/status.meep.social/g' {} \;
 popd
