e90c84c971f7ddd320d06a3344ff19d880c9a48d
Jason Stallings <jason@stallin.gs>
2021-06-01 09:10:02 -0500

    build: Correct update script.

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 pushd vue/node_modules/@starport
-# find . -type f -name '*.js' -exec sed -i '' 's/http:\/\/localhost:/http:\/\/159.65.103.150:/g' {} \;
+find . -type f -name '*.js' -exec sed -i 's/http:\/\/localhost:/http:\/\/159.65.103.150:/g' {} \;
 popd
 
 pushd vue/src/store/generated
-find . -type f -name '*.js' -exec sed -i '' 's/200000/50000000/g' {} \;
+find . -type f -name '*.js' -exec sed -i 's/200000/50000000/g' {} \;
 popd
