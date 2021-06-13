2894ebd7b6a38f7674d9ba8cab3700eff33cda23
Jason Stallings <jason@stallin.gs>
2021-06-13 10:02:42 -0500

    wip: Front end serving.

--- x/meep/client/rest/rest.go
+++ x/meep/client/rest/rest.go
@@ -28,6 +28,18 @@ func RegisterRoutes(clientCtx client.Context, r *mux.Router) {
 
 }
 
+// TODO: Finish HTML handler to serve frontend.
+// func serveHandler(clientCtx client.Context) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Println("butts")
+// 		// p := "." + r.URL.Path
+// 		// if p == "./" {
+// 		// 	p = "./static/index.html"
+// 		// }
+// 		// http.ServeFile(w, r, p)
+// 	}
+// }
+
 func registerQueryRoutes(clientCtx client.Context, r *mux.Router) {
 	// this line is used by starport scaffolding # 3
 	r.HandleFunc("/meep/tips/{id}", getTipHandler(clientCtx)).Methods("GET")
@@ -41,6 +53,9 @@ func registerQueryRoutes(clientCtx client.Context, r *mux.Router) {
 
 	r.HandleFunc("/meep/posts/{id}", getPostHandler(clientCtx)).Methods("GET")
 	r.HandleFunc("/meep/posts", listPostHandler(clientCtx)).Methods("GET")
+
+	// TODO: Finish HTML handler.
+	// r.HandleFunc("/meep/serve", serveHandler(clientCtx)).Methods("GET")
 
 }
 
