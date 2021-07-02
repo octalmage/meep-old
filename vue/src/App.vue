196b886ff7f48906afd7fe8b57fb72d15e03e175
Jason Stallings <jason@stallin.gs>
2021-07-02 18:29:54 -0500

    fix: init staking.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,39 +1,39 @@
 <template>
-	<div v-if="initialized">
-		<SpWallet ref="wallet" v-on:dropdown-opened="$refs.menu.closeDropdown()" />
-		<SpLayout>
-			<template v-slot:sidebar>
-				<Sidebar />
-			</template>
-			<template v-slot:content>
-				<router-view />
-			</template>
-		</SpLayout>
-	</div>
+  <div v-if="initialized">
+    <SpWallet ref="wallet" v-on:dropdown-opened="$refs.menu.closeDropdown()" />
+    <SpLayout>
+      <template v-slot:sidebar>
+        <Sidebar />
+      </template>
+      <template v-slot:content>
+        <router-view />
+      </template>
+    </SpLayout>
+  </div>
 </template>
 
 <style>
 body {
-	margin: 0;
+  margin: 0;
 }
 </style>
 
 <script>
-import './scss/app.scss'
-import '@starport/vue/lib/starport-vue.css'
-import Sidebar from './components/Sidebar'
+import "./scss/app.scss";
+import "@starport/vue/lib/starport-vue.css";
+import Sidebar from "./components/Sidebar";
 
 export default {
-	components: {
-		Sidebar
-	},
-	data() {
-		return {
-			initialized: false,
-			balances: [],
-		}
-	},
-	watch: {
+  components: {
+    Sidebar,
+  },
+  data() {
+    return {
+      initialized: false,
+      balances: [],
+    };
+  },
+  watch: {
     // whenever question changes, this function will run
     currentAccount: async function (newAccount) {
       if (newAccount) {
@@ -41,13 +41,13 @@ export default {
       }
     },
   },
-	computed: {
-		hasWallet() {
-			return this.$store.hasModule([ 'common', 'wallet'])
-		}
-	},
-	methods: {
-		async updateBalances() {
+  computed: {
+    hasWallet() {
+      return this.$store.hasModule(["common", "wallet"]);
+    },
+  },
+  methods: {
+    async updateBalances() {
       if (this.currentAccount) {
         await this.$store.dispatch("cosmos.bank.v1beta1/QueryAllBalances", {
           options: { subscribe: true, all: true },
@@ -61,20 +61,46 @@ export default {
         this.balances = [];
       }
     },
-	},
-	async created() {
-		await this.$store.dispatch('common/env/init');
+  },
+  async created() {
+		window.store = this.$store;
+		
+    await this.$store.dispatch("common/env/init", {  
+			apiNode: 'https://api.meep.social',
+  rpcNode: 'https://rpc.meep.social',
+  wsNode: 'wss://rpc.meep.social/websocket',
+	// 		apiNode: 'http://localhost:1317',
+  // rpcNode: 'http://localhost:26657',
+  // wsNode: 'ws://localhost:26657/websocket',
+  chainId: 'meep-1',
+  addrPrefix: 'meep',
+  sdkVersion: 'Stargate',} );
 
-		await this.$store.dispatch("octalmage.meep.meep/QueryPostAll",{options:{subscribe:true, all:true},params:{}})
-		await this.$store.dispatch("octalmage.meep.meep/QueryThreadAll",{options:{subscribe:true, all:true},params:{}})
-		await this.$store.dispatch("octalmage.meep.meep/QueryUsernameAll",{options:{subscribe:true, all:true},params:{}})
-		await this.$store.dispatch("octalmage.meep.meep/QueryTipAll",{options:{subscribe:true, all:true},params:{}})
 
-		this.initialized = true
-	},
-	// errorCaptured(err) {
-	// 	console.log(err)
-	// 	return false
-	// }
-}
+		await this.$store.dispatch('cosmos.staking.v1beta1/init');
+
+    await this.$store.dispatch("octalmage.meep.meep/QueryPostAll", {
+      options: { subscribe: true, all: true },
+      params: {},
+    });
+    await this.$store.dispatch("octalmage.meep.meep/QueryThreadAll", {
+      options: { subscribe: true, all: true },
+      params: {},
+    });
+    await this.$store.dispatch("octalmage.meep.meep/QueryUsernameAll", {
+      options: { subscribe: true, all: true },
+      params: {},
+    });
+    await this.$store.dispatch("octalmage.meep.meep/QueryTipAll", {
+      options: { subscribe: true, all: true },
+      params: {},
+    });
+
+    this.initialized = true;
+  },
+  // errorCaptured(err) {
+  // 	console.log(err)
+  // 	return false
+  // }
+};
 </script>
